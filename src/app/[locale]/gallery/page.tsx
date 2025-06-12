eb63e9807a5220afcbc34de7da358f0d320297ba
ohjsda <codeozxxx@gmail.com>
2025-06-12 19:52:45 +0900

    갤러리 이미지 확대 기능 개션, 갤러리 페이지에서 LightboxModal 컴포넌트에 likedItems 속성을 추가하여 좋아요 상태를 표시하도록 개선

--- src/app/[locale]/gallery/page.tsx
+++ src/app/[locale]/gallery/page.tsx
@@ -306,6 +306,7 @@ export default function GalleryPage() {
         filteredItems={filteredItems}
         toggleLike={toggleLike}
         currentId={currentId}
+        likedItems={likedItems}
       />
     </div>
   );
